246a2df413d501589f29d1f47d365eaa74c3dfd4
CristianMejias <163202037+CristianMejias@users.noreply.github.com>
2024-04-23 14:41:00 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -128,11 +128,13 @@ Pair * searchMap(HashMap * map,  char * key) {
 }
 
 Pair * firstMap(HashMap * map) {
-  for (long i = 0 ; i < map->capacity ; i++)
+  long i;
+  for (i = 0 ; i < map->capacity ; i++)
   {
     if (map->buckets[i]->key != NULL)
-      return map->buckets[i];
+      break;
   }
+  return map->buckets[i];
 }
 
 Pair * nextMap(HashMap * map) {
