656a098f3435edc589315901dfac63c6fad230b7
CristianMejias <163202037+CristianMejias@users.noreply.github.com>
2024-04-23 13:59:02 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -52,7 +52,7 @@ void insertMap(HashMap * map, char * key, void * value) {
   long pos = hash(key, map->capacity);
 
   //2) comprobar que posicion es valida
-  while (map->buckets[pos] != NULL && !is_equal(map->buckets[pos]->key, key)){
+  while (map->buckets[pos] != NULL && !is_equal(map->buckets[pos]->key, "-1")){
     //3) Como posicion no valida resolvemos colision
     pos = (pos + 1) % map->capacity;
     }
