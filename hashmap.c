2fd1c2289ad543e33ef81de3e6228c792caa41ed
CristianMejias <163202037+CristianMejias@users.noreply.github.com>
2024-04-23 14:47:18 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -140,6 +140,13 @@ Pair * firstMap(HashMap * map) {
 }
 
 Pair * nextMap(HashMap * map) {
-
-    return NULL;
+  for (long i = map->current ; i < map->capacity ; i++)
+  {
+    if (map->buckets[i] != NULL && map->buckets[i]->key != NULL)
+    {
+      map->current = i;
+      return map->buckets[i];
+    }
+  }
+  return NULL;
 }
