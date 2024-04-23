d087dbf9a77fd1ebb36b1cee21d36163ae9126b1
CristianMejias <163202037+CristianMejias@users.noreply.github.com>
2024-04-23 13:05:04 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -52,7 +52,7 @@ void enlarge(HashMap * map) {
 
 
 HashMap * createMap(long capacity) {
-  HashMap *nuevoHashMap = (HashMap *) malloc(capacity * sizeof(HashMap));
+  HashMap *nuevoHashMap = (HashMap *) malloc(sizeof(HashMap));
   if (nuevoHashMap == NULL)
     exit(EXIT_FAILURE);
   
